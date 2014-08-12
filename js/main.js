91192a0270043664f8a143c8308c0d48de61a667
Michal Budzynski <michal@virtualdesign.pl>
2014-08-12 16:15:46 +0200

    [ATLASV] - :fu: separate logic to different files

---
+++ js/main.js
@@ -0,0 +1,51 @@
+'use strict';
+
+var AV = AV || {};
+
+AV.main = (function() {
+   var canvas = document.getElementById('canvas');
+   var ctx = canvas.getContext('2d');
+
+  var drawGrid = function() {
+    var start = 0;
+    // vertical
+    for (var i = 0; i < AV.consts.cellsX; i++) {
+      ctx.beginPath();
+      ctx.moveTo(start-0.5, 0);
+      ctx.lineTo(start-0.5, AV.consts.totalHeight);
+      ctx.stroke();
+      start += AV.consts.cellSize;
+    }
+
+    start = 0;
+    //horizontal
+    for (i = 0; i < AV.consts.cellsY; i++) {
+      ctx.beginPath();
+      ctx.moveTo(0, start-0.5);
+      ctx.lineTo(AV.consts.totalWidth, start-0.5);
+      ctx.stroke();
+      start += AV.consts.cellSize;
+    }
+  };
+  var init = function() {
+    canvas.width = AV.consts.totalWidth;
+    canvas.height = AV.consts.totalHeight;
+    tick();
+  };
+
+  var clearCanvas = function(){
+    ctx.clearRect(0, 0, AV.consts.totalWidth, AV.consts.totalHeight);
+    drawGrid();
+  };
+
+  var tick = function() {
+    clearCanvas();
+    //requestAnimationFrame(tick);
+  };
+
+  return {
+    init: init
+  };
+})();
+
+AV.main.init();
