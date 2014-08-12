e4718eba02da306f035a3dc5e51eebc3fb0225c9
Michal Budzynski <michal@virtualdesign.pl>
2014-08-12 16:16:12 +0200

    [ATLASV] - :memo: lint fixes

--- js/consts.js
+++ js/consts.js
@@ -1,9 +1,11 @@
+'use strict';
+
 var AV = AV || {};
 
 AV.consts = {
   cellSize: 30,
   cellsX: 10,
   cellsY: 15,
-  get totalWidth() { return AV.consts.cellSize * AV.consts.cellsX },
-  get totalHeight() { return AV.consts.cellSize * AV.consts.cellsY }
-}
+  get totalWidth() { return AV.consts.cellSize * AV.consts.cellsX; },
+  get totalHeight() { return AV.consts.cellSize * AV.consts.cellsY; }
+};
