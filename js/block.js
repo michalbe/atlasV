8618c91a7046e10ea7221cf631a8c30b9c5c7d3d
Michal Budzynski <michal@virtualdesign.pl>
2014-08-12 16:44:31 +0200

    [ATLASV] - :sushi: block movement

--- js/block.js
+++ js/block.js
@@ -5,13 +5,16 @@ var AV = AV || {};
 AV.block = function(x, y, color) {
   this.x = x;
   this.y = y;
-  this.speedY = 1;
+  this.speedY = 4;
   this.speedX = 0;
   this.color = color;
 };
 
-AV.block.prototype.update = function(){
-  this.y += this.speedY;
+AV.block.prototype.update = function() {
+  if (this.y < AV.consts.totalHeight - AV.consts.cellSize) {
+    this.y += this.speedY;
+  }
+
   this.x += this.speedX;
 };
 
